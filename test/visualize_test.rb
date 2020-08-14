41e37ea54b5dbc13b6921f312586108f5be172c9
Ellen Marie Dash <me@duckie.co>
2020-08-14 19:35:16 -0400

    Fix warning when running tests.

--- test/visualize_test.rb
+++ test/visualize_test.rb
@@ -136,7 +136,7 @@ class VisualizeTest < Minitest::Test
 
   def run_bundler_command(command)
     output = nil
-    Bundler.with_clean_env do
+    Bundler.with_unbundled_env do
       output, status = Open3.capture2e(command)
 
       raise StandardError, "#{command} failed: #{output}" unless status.success?
